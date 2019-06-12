e6a968a39069ed91cb9f504cccdab586bd13dc9e
KiLuy <k.luijken@lumc.nl>
2019-06-12 23:19:05 +0200

    Updates

---
+++ precache-manifest.ee01502e1a8a6544603f34564654a892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d89800a12e0c20e110447a28a64cc7",
+    "url": "/MHPredict_Dialogue/index.html"
+  },
+  {
+    "revision": "1c906841adb79e1c22a3",
+    "url": "/MHPredict_Dialogue/static/css/main.bdcc40fc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/MHPredict_Dialogue/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1c906841adb79e1c22a3",
+    "url": "/MHPredict_Dialogue/static/js/main.dc20598e.chunk.js"
+  },
+  {
+    "revision": "217043b31f915d023561",
+    "url": "/MHPredict_Dialogue/static/js/runtime~main.0c215b11.js"
+  }
+]);
